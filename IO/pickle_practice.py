8177d00a77ec130c54f7975a82108a3dd177aa2a
pup-paw <wldusdl0310@gmail.com>
2021-08-25 21:43:34 +0900

    7. 파일 입출력

--- IO/pickle_practice.py
+++ IO/pickle_practice.py
@@ -5,7 +5,7 @@ import pickle
 # pickle.dump(profile, profile_file)
 # profile_file.close()
 
-profile_file = open("profile.pickle", "rb")
-profile = pickle.load(profile_file) #file에 있는 정보를 profile에 불러오기
+profile_file = open("IO/profile.pickle", "rb")
+profile = pickle.load(profile_file)  # file에 있는 정보를 profile에 불러오기
 print(profile)
 profile_file.close()
